a14c85c5c502edf84f5a8b8b7dba4075160ed77c
limbugoogle <limbugoogle@outlook.com>
2022-03-06 23:15:16 +0000

    clean up

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 require("plugins")
-require("treesitter-config")
+require("keybindings")
 require("options")
+require("nvim-tree-config")
 require("lualine-config")
 require("bufferline-config")
+require("treesitter-config")
 --Lua:
-require("nvim-tree-config")
 require("whichkey-config")
-require("keybindings")
 vim.cmd[[colorscheme nord]]
