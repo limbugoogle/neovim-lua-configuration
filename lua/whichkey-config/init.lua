79b4329982d391223abf5dea7eaf568cf1343d3e
limbugoogle <limbugoogle@outlook.com>
2022-03-07 09:42:49 +0000

    update which key with telescope config

--- lua/whichkey-config/init.lua
+++ lua/whichkey-config/init.lua
@@ -1,11 +1,12 @@
 local wk = require("which-key")
 
 local mappings = {
-  q = {":q<cr>", "Quit"},
-  Q = {":wq<cr>", "Save & Quit"},
-  w = {":w<cr>", "Save"},
   x = {":bdelete<cr>", "Close"},
-  E = {":e ~/.config/nvim/init.lua<cr>", "Edit config"}
+  E = {":e ~/.config/nvim/init.lua<cr>", "Edit config"},
+  f = {":Telescope find_files<cr>", "Telescope Find Files"},
+  r = {":Telescope live_grep<cr>", "Telescope Live Grep"},
+  b = {":Telescope buffers<cr>", "Telescope buffers"},
+  t = {":Telescope help_tags<cr>", "Telescope Help Tags"}
 }
 local opts = {prefix = '<leader>'}
 wk.register(mappings, opts)
