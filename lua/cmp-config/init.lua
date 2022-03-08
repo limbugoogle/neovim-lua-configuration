8a1c01dbe381c16c8c5f1de930d59a001e1be909
limbugoogle <limbugoogle@outlook.com>
2022-03-08 06:20:04 +0000

    change diagnotic symbols in the sign column

--- lua/cmp-config/init.lua
+++ lua/cmp-config/init.lua
@@ -94,5 +94,8 @@ cmp.setup({
     capabilities = capabilities
   }
 
-
-
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
